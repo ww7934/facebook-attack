12d52d87a9b6a5fdbfc1dc7db8d9148adc18cc7f
ww7934 <131043463+ww7934@users.noreply.github.com>
2024-01-26 20:35:38 +0200

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,18 @@
+# Evil Python 2 code with Syrian flag
+import requests
+
+def facebook_attack(victim_id):
+    # Execute your attacks on the Facebook page using the given ID
+    print("Successful attack on Facebook page using ID: " + str(victim_id))
+    print("Adding a Syrian touch... 🇸🇾")
+
+if _name_ == "_main_":
+    # Use this line to check if the tool is running in Kali Linux
+    print("To run the tool, type cd and folder name, then ls")
+
+    # Enter the victim's ID after running the tool
+    victim_id = raw_input("Enter the victim's ID: ")
+
+    # Start the attack after entering the ID
+    facebook_attack(victim_id)
+    print("Tool interface now displays the Syrian flag! 🇸🇾")
